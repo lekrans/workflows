4dcc33f24f7cd9ab32b824edda6598c74cbfc0f1
Michael Lekrans <michael.lekrans@chyronhego.com>
2017-05-22 09:07:07 +0200

    added default task

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -75,7 +75,6 @@ var $, fill;
 
 $ = require('jquery');
 
-
 (fill = function(item) {
   return $('.tagline').append("" + item);
 })('The most creative minds in Art');
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -2,7 +2,6 @@ var $, fill;
 
 $ = require('jquery');
 
-
 (fill = function(item) {
   return $('.tagline').append("" + item);
 })('The most creative minds in Art');
