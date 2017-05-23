4c45350cab051d793678059d2f049c10f2fc7f45
Michael Lekrans <michael.lekrans@chyronhego.com>
2017-05-23 16:22:49 +0200

    gulp minify HTML

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
     connect = require('gulp-connect'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
@@ -67,7 +68,9 @@ gulp.task('compass', function() {
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('builds/development/*.html')
+  .pipe(gulpif(env === 'production', minifyHTML()))
+  .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
   .pipe(connect.reload())
 })
 
@@ -75,7 +78,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-  gulp.watch(htmlSources, ['html'])
+  gulp.watch('builds/development/*.html', ['html'])
 })
 
 gulp.task('connect', function() {
@@ -85,7 +88,7 @@ gulp.task('connect', function() {
   })
 })
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
 
 
